0b988541cf7dbe057d96473b38e4c11061a456ca
nicolas020408 <nicolas020408@gmail.com>
2024-10-23 14:56:36 -0300

    fazendo as torretas se mexerem pra matar o inimigo

--- tower/Assets/turret.cs
+++ tower/Assets/turret.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class turret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Reference")]
+    [SerializeField] private Transform turretRotationPoint;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 5f;
+
+    private void OnDrawGizmosSelected()
     {
-        
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+
     }
+    
+        
+    
 }
