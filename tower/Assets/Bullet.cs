79162c23f971bf4d27d79f2b30d706df7abdfcb3
nicolas020408 <nicolas020408@gmail.com>
2024-10-25 13:00:11 -0300

    fazendo o dano no inimigo

--- tower/Assets/Bullet.cs
+++ tower/Assets/Bullet.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Refereences")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float bulletSpeed = 5f;
+
+    private Transform target;
+
+    public void SetTarget(Transform _target)
     {
-        
+        target = _target; 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (!target) return;
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        rb.velocity = direction * bulletSpeed;
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // TAKE Health From Enemy
+        Destroy(gameObject);
+    }   
 }
